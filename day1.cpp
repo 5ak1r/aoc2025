075dacb9582c3f1bbdb6bfe6900c01b8954e6225
Rikas <s.azimkar1@newcastle.ac.uk>
2025-12-01 15:43:15 +0000

    d1p2

--- day1.cpp
+++ day1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <utility>
+#include <cmath>
 
 #include "readfile.hpp"
 
@@ -15,9 +17,36 @@ int partOne() {
     int amount = std::stoi(line.substr(1));
 
     dir == 'L' ? start -= amount : start += amount;
-    start = ((start % 100) + 100) % 100;
 
+    start = ((start % 100) + 100) % 100;
+    
     if(start == 0) zeroes++;
+  }
+
+  return zeroes;
+}
+
+int partTwo() {
+  file.clear();
+  file.seekg(0, std::ios::beg);
+
+  int start = 50;
+  int zeroes = 0;
+ 
+  std::string line;
+  while(std::getline(file, line)) {
+    char dir = line[0];
+    int amount = std::stoi(line.substr(1));
+
+    for(int i = 0; i < amount; i++) {
+      dir == 'L' ? start -= 1: start += 1;
+
+      if(start == 100 || start == 0) {
+        zeroes++;
+      }
+
+      start = ((start % 100) + 100) % 100;
+    }
   }
 
   return zeroes;
@@ -25,4 +54,5 @@ int partOne() {
 
 int main() {
   std::cout << partOne() << std::endl;
+  std::cout << partTwo() << std::endl;
 }
